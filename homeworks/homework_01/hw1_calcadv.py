c532b69355d31d85382d0349c7edeedf70f59550
LoskutovPD <48155308+LoskutovPD@users.noreply.github.com>
2019-03-07 04:19:07 +0300

    Update hw1_calcadv.py

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -1,13 +1,29 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # coding: utf-8
 
 
 def advanced_calculator(input_string):
-    '''
-    Калькулятор на основе обратной польской записи.
-    Разрешенные операции: открытая скобка, закрытая скобка,
-     плюс, минус, умножить, делить
-    :param input_string: строка, содержащая выражение
-    :return: результат выполнение операции, если строка валидная - иначе None
-    '''
-    raise NotImplementedError
+    if len(input_string) == 0:
+        return None
+    data_num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', \
+                '0', '.', '+', '-', '*', '/', '(', ')', ' ', '\t']
+    for x in input_string:
+        if x not in data_num:
+            return None
+    if input_string[0]  == '-' or input_string[0] == '+':
+        input_string = "0" + input_string
+    temp = input_string.replace("--", "+")
+    while "++" in temp:
+        temp = temp.replace("++", "+")
+    temp= temp \
+    .replace("(", " ( ").replace(")", " ) ").replace("+", " + ") \
+    .replace("-", " - ").replace("*", " * ").replace("/", " / ")
+    temp = " ".join(temp.split())
+    if "* *" in temp or "/ /" in temp or "/ *" in temp or \
+    "* /" in temp or "+ *" in temp or "- *" in temp or\
+    "+ /" in temp or "- /" in temp or "( )" in temp:
+        return None
+    try:
+        return eval(temp)
+    except:
+        return None
