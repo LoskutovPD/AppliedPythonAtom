afc4dcd851c16a26b6c64d9dbb670c82a925d040
LoskutovPD <48155308+LoskutovPD@users.noreply.github.com>
2019-03-07 04:24:09 +0300

    Update hw1_calcadv.py

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -5,23 +5,23 @@
 def advanced_calculator(input_string):
     if len(input_string) == 0:
         return None
-    data_num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', \
+    data_num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 
                 '0', '.', '+', '-', '*', '/', '(', ')', ' ', '\t']
     for x in input_string:
         if x not in data_num:
             return None
-    if input_string[0]  == '-' or input_string[0] == '+':
+    if input_string[0] == '-' or input_string[0] == '+':
         input_string = "0" + input_string
     temp = input_string.replace("--", "+")
     while "++" in temp:
         temp = temp.replace("++", "+")
-    temp= temp \
-    .replace("(", " ( ").replace(")", " ) ").replace("+", " + ") \
-    .replace("-", " - ").replace("*", " * ").replace("/", " / ")
+    temp = temp.replace("(", " ( ").replace(")", " ) ")
+    temp = temp.replace("+", " + ").replace("-", " - ")
+    temp = temp.replace("*", " * ").replace("/", " / ")
     temp = " ".join(temp.split())
-    if "* *" in temp or "/ /" in temp or "/ *" in temp or \
-    "* /" in temp or "+ *" in temp or "- *" in temp or\
-    "+ /" in temp or "- /" in temp or "( )" in temp:
+    if "* *" in temp or "/ /" in temp or "/ *" in temp \
+        or "* /" in temp or "+ *" in temp or "- *" in temp \
+        or "+ /" in temp or "- /" in temp or "( )" in temp:
         return None
     try:
         return eval(temp)
