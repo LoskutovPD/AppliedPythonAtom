91428d04d3acf7b274463e885e71b1d8281aaa0e
LoskutovPD <48155308+LoskutovPD@users.noreply.github.com>
2019-03-12 01:04:47 +0300

    Update vkposter.py

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-from homeworks.homework_02.heap import MaxHeap
-from homeworks.homework_02.fastmerger import FastSortedListMerger
-
-
 class VKPoster:
 
     def __init__(self):
@@ -39,13 +31,14 @@ class VKPoster:
         if follower_user_id in self.users:
             self.users[follower_user_id]['follow'].append(followee_user_id)
         else:
-            self.users[follower_user_id] = {'publicated': [], 'follow': [followee_user_id]}
+            self.users[follower_user_id] = {'publicated': [],
+                                            'follow': [followee_user_id]}
 
     def get_recent_posts(self, user_id: int, k: int) -> list:
         i, return_list = 0, []
         for post in [i for i in self.posts.keys()][::-1]:
             if i >= k:
-                return return_list
+                return return_list.sort(reverse=True)
             if self.posts[post]['author'] in self.users[user_id]['follow']:
                 return_list.append(post)
                 i += 1
@@ -53,7 +46,8 @@ class VKPoster:
     def get_most_popular_posts(self, k: int) -> list:
         result, cur = [], k
         temp_posts = self.invert_dict(self.posts)
-        second_tmp = [(j, temp_posts[j]) for j in [i for i in temp_posts.keys()][::-1]]
+        second_tmp = [(j, temp_posts[j]) for j in [i for i in
+                                                   temp_posts.keys()][::-1]]
         second_tmp.sort(reverse=True)
         for tmp in second_tmp:
             if isinstance(tmp, list):
