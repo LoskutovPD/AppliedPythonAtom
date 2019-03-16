b527d0226939d7778aebd338d704a43993ec9e4e
LoskutovPD <48155308+LoskutovPD@users.noreply.github.com>
2019-03-16 23:12:53 +0300

    Update hw3_revert_linked_list.py

--- homeworks/homework_03/hw3_revert_linked_list.py
+++ homeworks/homework_03/hw3_revert_linked_list.py
@@ -9,4 +9,11 @@ def revert_linked_list(head):
     :return: new_head: LLNode
     """
     # TODO: реализовать функцию
-    raise NotImplementedError
+    return revert(head.next_node, head)
+
+
+def revert(next_v, prev=None):
+    if next_v is None:
+        return prev
+    next_v.next_node, tmp = prev, next_v.next_node
+    return revert(tmp, next_v)
