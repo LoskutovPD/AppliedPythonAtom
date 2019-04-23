dc86a39be4ea83ce4b0e8d46de232e4e1317d1e2
LoskutovPD <48155308+LoskutovPD@users.noreply.github.com>
2019-04-23 07:33:55 +0300

    new

--- homeworks/homework_07_ml/logreg/logistic_regression.py
+++ homeworks/homework_07_ml/logreg/logistic_regression.py
@@ -6,43 +6,39 @@ import numpy as np
 
 
 class LogisticRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5):
-        """
-        LogReg for Binary case
-        :param lambda_coef: constant coef for gradient descent step
-        :param regulatization: regularizarion type ("L1" or "L2") or None
-        :param alpha: regularizarion coefficent
-        """
-        pass
+    def __init__(self, lambda_coef=1.0, regulatization=None,
+                 c=2, n_iter = 100):
+        self.lambda_coef = lambda_coef
+        self.regul = regulatization
+        self.alpha = 1 / c
+        self.iter = n_iter
+        self.train = False
 
     def fit(self, X_train, y_train):
-        """
-        Fit model using gradient descent method
-        :param X_train: training data
-        :param y_train: target values for training data
-        :return: None
-        """
-        pass
+        self.train = True
+        label = np.array([y_train.T]).T
+        self.w = np.random.randn(X_train.shape[1], 1)
+        if self.regul == "L2":
+            tmp = np.linalg.norm(self.w, ord=1) * self.lambda_coef
+        elif self.regul == "L1":
+            tmp = np.linalg.norm(self.w) * 2 * self.lambda_coef
+        else:
+            tmp = 0
+        for _ in range(self.iter):
+            pred = self.predict_proba(X_train)
+            grad = self.alpha * tmp + ((X_train.T @ (pred - label)) /
+                                       X_train.shape[0])
+            self.w -= grad * self.lambda_coef
 
-    def predict(self, X_test):
-        """
-        Predict using model.
-        :param X_test: test data for predict in
-        :return: y_test: predicted values
-        """
-        pass
+    def predict(self, X):  #around
+        assert self.train, 'dummy attempt'
+        return np.around(self.predict_proba(X))
 
-    def predict_proba(self, X_test):
-        """
-        Predict probability using model.
-        :param X_test: test data for predict in
-        :return: y_test: predicted probabilities
-        """
-        pass
+    def predict_proba(self, X):  #sigmoid
+        assert self.train, 'dummy attempt'
+        tpm = -X @ self.w
+        return 1 / (1 + np.exp(tpm))
 
     def get_weights(self):
-        """
-        Get weights from fitted linear model
-        :return: weights array
-        """
-        pass
+        assert self.train, 'dummy attempt'
+        return self.w
--- homeworks/homework_07_ml/metrics/metrics.py
+++ homeworks/homework_07_ml/metrics/metrics.py
@@ -3,53 +3,66 @@
 
 
 import numpy as np
+from pandas import DataFrame as pddf
 
 
 def logloss(y_true, y_pred):
-    """
-    logloss
-    :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated probabilities
-    :return: loss
-    """
-    pass
+    if y_true.shape != y_pred.shape:
+        raise AssertionError
+    return (-1/(y_true.shape[0] +
+                0.00000001)) * sum(y_true*np.log(y_pred) +
+                                   (1 - y_true)*np.log(1 - y_pred))
+
+
+def mke_cnf_matrx(y_true, y_hat):
+    matrix = np.zeros([2, 2])
+    for tr, hat in zip(y_true, y_hat):
+        if tr + hat == 2:
+            matrix[0, 0] += 1  # [0, 0] = TP
+        elif tr + hat == 0:
+            matrix[1, 1] += 1  # [1, 1] = TN
+        else:
+            if tr == 1:
+                matrix[1, 0] += 1  # [1, 0] = FN
+            else:
+                matrix[0, 1] += 1  # [0, 1] = FP
+    return matrix
 
 
 def accuracy(y_true, y_pred):
-    """
-    Accuracy
-    :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated class values
-    :return: loss
-    """
-    pass
+    if y_true.shape != y_pred.shape:
+        raise AssertionError
+    matr = mke_cnf_matrx(np.around(y_true), np.around(y_pred))
+    return (matr[0, 0] + matr[1, 1])/matr.sum()
 
 
 def presicion(y_true, y_pred):
-    """
-    presicion
-    :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated class values
-    :return: loss
-    """
-    pass
+    if y_true.shape != y_pred.shape:
+        raise AssertionError
+    matr = mke_cnf_matrx(np.around(y_true), np.around(y_pred))
+    return matr[0, 0] / (matr[0, 0] + matr[0, 1])
 
 
 def recall(y_true, y_pred):
-    """
-    presicion
-    :param y_true: vector of truth (correct) class values
-    :param y_hat: vector of estimated class values
-    :return: loss
-    """
-    pass
+    if y_true.shape != y_pred.shape:
+        raise AssertionError
+    matr = mke_cnf_matrx(np.around(y_true), np.around(y_pred))
+    return matr[0, 0] / (matr[0, 0] + matr[1, 0])
 
 
 def roc_auc(y_true, y_pred):
-    """
-    roc_auc
-    :param y_true: vector of truth (correct) target values
-    :param y_hat: vector of estimated probabilities
-    :return: loss
-    """
-    pass
+    if y_true.shape != y_pred.shape:
+        raise AssertionError
+    df = pddf({'label': y_test, 'pred': clf.predict_proba(X_test)[:, 1]})
+    df = df.sort_values(by='pred', )[:: -1]
+    x1 = y_test.sum()
+    x2 = y_test.shape[0] - y_test.sum()
+    auc = 0
+    tmp = np.array([0., 0.])
+    for y in df['label']:
+        if y == 0:
+            tmp[0] += 1 / x2
+            auc += tmp[1] / x2
+        else:
+            tmp[1] += 1/x1
+    return auc
